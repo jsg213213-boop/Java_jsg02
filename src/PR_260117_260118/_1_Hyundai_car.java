70eaa330dcb0ff6127473cd566a47c68b37e4c08
정성규 <jsg213213@naver.com>
2026-01-17 18:21:58 +0900

    260117 정성규

--- src/PR_260117_260118/_1_Hyundai_car.java
+++ src/PR_260117_260118/_1_Hyundai_car.java
@@ -21,7 +21,7 @@ class Car {
 }
 
 public class _1_Hyundai_car {
-    public static void main(String[] args) {
+    static void main() {
 
         Car car01 = new Car("스타리아", 2026, "승합차");
         Car car02 = new Car("코나", 2026, "소형 SUV");
